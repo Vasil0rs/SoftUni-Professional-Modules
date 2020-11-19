4cda5aec3e550b1f5c790a927615c58ecc8b508c
vasil vasilev <vasil0rs@gmail.com>
2020-11-19 15:40:55 +0000

    Animal

---
+++ Java-OOP-HomeWork/Animal/Cat.java
@@ -0,0 +1,14 @@
+package Animal;
+
+public class Cat extends Animal {
+
+    protected Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%n" +
+                "MEEOW",getName(),getFavouriteFood());
+    }
+}
