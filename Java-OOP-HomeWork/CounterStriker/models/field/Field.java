6634c9dee020a436c2237b65e15b79cfaf17dc88
vasil vasilev <vasil0rs@gmail.com>
2020-12-01 09:18:21 +0000

    CounterStriker

---
+++ Java-OOP-HomeWork/CounterStriker/models/field/Field.java
@@ -0,0 +1,10 @@
+package CounterStriker.models.field;
+
+import CounterStriker.models.players.Player;
+
+import java.util.Collection;
+
+public interface Field {
+
+    String start(Collection<Player> players);
+}
